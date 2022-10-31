2b7d62b216fb547f24170eabe5d4279407476de0
Colleen Garvey <colleenellengarvey@gmail.com>
2022-10-31 16:55:31 -0400

    socket installed and server update

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hello from our node script');
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    io.emit('message', msg);
+  });
+});
+
+server.listen(8080, function() {
+  console.log('Chat server listening on port 8080!');
+})
